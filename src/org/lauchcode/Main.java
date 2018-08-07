22d69c68df6e330607074896cb7f565b99873790
Kuba <jjkorniluk@wp.pl>
2018-08-07 10:51:54 +0200

    Różne metody zaokrąglania uporządkowanie kodu.

--- src/org/lauchcode/Main.java
+++ src/org/lauchcode/Main.java
@@ -8,26 +8,32 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         String inputName;
         String inputSurname;
-
-
-
-        inputSurname = scanner.next();
-        System.out.println("Podaj Swój wiek:");
-        String inputAge;
-        inputAge = scanner.next();
-        float procesAge = Float.parseFloat(inputAge);
-        String outAge = String.format ("%.0f", procesAge);
-        System.out.println("Podaj Swóją wzrost:");
         String inputTall;
+        int inputAge;
+        float inputWeight;
+
+        System.out.println("Podaj Swoje imie: ");
+        inputName = scanner.next();
+
+        System.out.println("Podaj Swoje nazwisko: ");
+        inputSurname = scanner.next();
+
+        System.out.println("Podaj Swój wiek (liczba całkowita):");
+        inputAge = scanner.nextInt();
+
+
+        System.out.println("Podaj Swóją wzrost(liczby niecałkowite po kropce):"); //Podawać z kropką jako znak dziesiętny
         inputTall = scanner.next();
-        float procesTall = Float.parseFloat(inputTall); // Test github
+        float procesTall = Float.parseFloat(inputTall);
         String outTall = String.format ("%.1f", procesTall);
-        System.out.println("Podaj Swóją wagę:");
-        String inputWeight;
-        inputWeight = scanner.next();
-        float procesWeight = Float.parseFloat(inputWeight);
-        String outWeight = String.format ("%.1f", procesWeight);
-        System.out.println("Nazywasz się : "+inputName+" "+inputSurname+",masz "+outAge+" lat, masz "+outTall+ " cm wzrosu oraz ważysz "+ outWeight+ " kg" );
+
+        System.out.println("Podaj Swóją wagę(liczby niecałkowite po przecinku):");   //Podawać z przecinkiem jako znak dziesiętny
+        inputWeight = scanner.nextFloat();
+
+
+        System.out.print("Nazywasz się : "+inputName+" "+inputSurname+",masz "+inputAge+" lat, masz "+outTall+ " cm wzrosu oraz ważysz ");
+        System.out.printf("%.1f", inputWeight);
+        System.out.print(" kg");
 
 
     }
